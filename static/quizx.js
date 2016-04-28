690bb74507ddbf10e84fffc3650bc0b49c3cd54e
Michael Krause <mekva6274@gmail.com>
2016-04-27 22:16:50 -0400

    updated gitignore, started some namespacing

--- static/quizx.js
+++ static/quizx.js
@@ -1,9 +1,14 @@
-function log(toAdd){
-	document.getElementById("logging").innerHTML += toAdd;
-}
-var socket = io.connect();
-socket.on('init', function (data) {
-	//log(data);
-	console.log(data);
-	socket.emit('my other event', { my: 'data' });
-});
+var quizx = {
+	log: function(toAdd){
+		document.getElementById("logging").innerHTML += toAdd;
+	},
+	var socket = io.connect();
+	socket.on('init', function (data) {
+		//log(data);
+		console.log(data);
+		socket.emit('my other event', { my: 'data' });
+	});
+};
+
+
+//OR SOMETHING LIKE THAT
